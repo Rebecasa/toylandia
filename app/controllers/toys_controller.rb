251540165121133afb71a1547a8225b6ed4e4d88
Jadwiga <40387014+JadwigaFR@users.noreply.github.com>
2018-11-20 14:19:17 +0100

    Revert "Authorization"

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,26 +1,19 @@
 class ToysController < ApplicationController
-  # Any visitor can access the Toys index and the individual show pages
-  skip_before_action :authenticate_user!, only: [:index, :show]
-
   def index
-    @toys = policy_scope(Toy).order(created_at: :desc)
-    authorize @toys
+    @toys = Toy.all
   end
 
   def show
     @toy = Toy.find(params[:id])
-    authorize @toy
   end
 
   def new
     @toy = Toy.new
-    authorize @toy
   end
 
   def create
     @toy = Toy.new(toy_params)
     @toy.user = current_user
-    authorize @toy
     if @toy.save
       redirect_to @toy
     else
@@ -30,13 +23,11 @@ class ToysController < ApplicationController
 
   def edit
     @toy = Toy.find(params[:id])
-    authorize @toy
   end
 
   def update
     @toy = Toy.find(params[:id])
     @toy.update(toy_params)
-    authorize @toy
     if @toy.save
       redirect_to @toy
     else
