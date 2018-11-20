f5181372195790f67f73b8a9ed2fad24b4ae20fc
nacho lopez <nachete-1995@hotmail.com>
2018-11-20 11:42:49 +0100

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Toy.destroy_all
+puts 'Creating adorable stuff...'
+
+bala = User.new( email: 'balatheballerina@dogs.com', password: '123456789')
+bala.save!
+
+bunny = User.new( email: 'bunnythedoghater@dogs.com', password: '123456789')
+bunny.save!
+
+lapinou = Toy.new(user: bunny, name: 'lapinou', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'creepy')
+lapinou.save!
+
+poupi = Toy.new(user: bunny,name: 'poupi', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'cuddly')
+poupi.save!
+
+beisan = Toy.new(user: bala, name: 'beisan', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'creepy')
+beisan.save!
+
+conejito = Toy.new(user: bala, name: 'conejito', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'educational')
+conejito.save!
+
+puts 'Finished!'
+
