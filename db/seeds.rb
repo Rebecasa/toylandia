d918e1669ceea6aaa68dbf28d432508e722cb87b
Jadwiga Coumert <jadwiga.coumert@gmail.com>
2018-11-21 10:36:39 +0100

    Defined the card styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 User.destroy_all
 Toy.destroy_all
 puts 'Creating adorable stuff...'
@@ -15,19 +16,19 @@ bala.save!
 bunny = User.new( email: 'bunnythedoghater@dogs.com', password: '123456789')
 bunny.save!
 
-lapinou = Toy.new(user: bunny, name: 'lapinou', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'creepy')
+lapinou = Toy.new(user: bunny, name: 'lapinou', description: 'Lapinou is a very cuddly plushtoy. You can snuggle with it all night and rub your nose into its cute little ears', location: 'Bunny street 1307 Toylandia', price: 5, category:'cuddly')
 lapinou.remote_photo_url = 'https://res.cloudinary.com/blueolive/image/upload/v1542710715/tqbjt0z5fegfxa4mevmt.jpg'
 lapinou.save!
 
-poupi = Toy.new(user: bunny, name: 'poupi', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'cuddly')
+poupi = Toy.new(user: bunny, name: 'poupi', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 9, category:'creepy')
 poupi.remote_photo_url = 'https://res.cloudinary.com/blueolive/image/upload/v1542710518/e6n3cpr5iyjchxzt23z4.jpg'
 poupi.save!
 
-beisan = Toy.new(user: bala, name: 'beisan', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'creepy')
+beisan = Toy.new(user: bala, name: 'beisan', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 3, category:'creepy')
 beisan.remote_photo_url = 'https://res.cloudinary.com/blueolive/image/upload/v1542719080/doll.jpg'
 beisan.save!
 
-conejito = Toy.new(user: bala, name: 'conejito', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 5, category:'educational')
+conejito = Toy.new(user: bala, name: 'conejito', description: 'blablablabla', location: 'Bunny street 1307 Toylandia', price: 2, category:'educational')
 conejito.remote_photo_url = 'https://res.cloudinary.com/blueolive/image/upload/v1542719181/-.jpg'
 conejito.save!
 
