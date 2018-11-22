5dece83b66c1482706cfd98a237ef67fc47daeec
Rebeca Souza <rebi_britto@hotmail.com>
2018-11-22 16:04:43 +0100

    implemented js on bookings index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import { showForm, hideForm } from './reviews';
 import { formatCurrency } from './price';
+import {} from './booking';
 
 showForm();
 hideForm();
