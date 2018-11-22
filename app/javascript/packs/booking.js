0c9fa0297eebe8a0a71e4949863ad72706c3dc56
Rebeca Souza <rebi_britto@hotmail.com>
2018-11-22 17:44:55 +0100

    displaying my toys on bookings index

--- app/javascript/packs/booking.js
+++ app/javascript/packs/booking.js
@@ -2,13 +2,16 @@
     const bookings = document.getElementById("bookings");
     const rentalsBtn = document.querySelector(".rentals-btn");
     const rentals = document.getElementById("rentals");
+    const mytoysBtn = document.querySelector(".mytoys-btn");
+    const mytoyss = document.getElementById("mytoys");
 
         bookingsBtn.addEventListener("click", (event) =>{
           if ( bookings.style.display == "none" ){
            bookings.style.display = "block";
             if ( bookings.style.display == "block" ){
-           rentals.style.display = "none";
-         }
+               rentals.style.display = "none";
+               mytoys.style.display = "none";
+            }
           } else {
          bookings.style.display = "none";
           }
@@ -19,9 +22,22 @@
                rentals.style.display = "block";
                 if  (rentals.style.display == "block" ){
                   bookings.style.display = "none";
+                  mytoys.style.display = "none";
                 }
               } else {
              rentals.style.display = "none";
+              }
+        });
+
+            mytoysBtn.addEventListener("click", (event) =>{
+              if ( mytoys.style.display == "none"){
+               mytoys.style.display = "block";
+                if  (mytoys.style.display == "block" ){
+                  bookings.style.display = "none";
+                  rentals.style.display = "none";
+                }
+              } else {
+             mytoys.style.display = "none";
               }
         });
 
